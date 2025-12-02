5ac201403d2ff486c19682e7ad9168a46396c17b
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-02 15:22:16 +0530

    add vanilla autoencoder

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from vae!")
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class AutoEncoder(nn.Module):
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=20):
+        super().__init__()
 
-if __name__ == "__main__":
-    main()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, latent_dim)
+
+        self.fc3 = nn.Linear(latent_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, input_dim)
+
+    def encode(self, x):
+        h = F.relu(self.fc1(x))
+        z = self.fc2(h)
+        return z
+    
+    def decode(self, z):
+        h = F.relu(self.fc3(z))
+        x_recon = torch.sigmoid(self.fc4(h))
+        return x_recon
+    
+    def forward(self, x):
+        z = self.encode(x)
+        x_recon = self.decode(z)
+        return x_recon, z
